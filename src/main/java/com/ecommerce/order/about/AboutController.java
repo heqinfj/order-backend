e2115a0ebd9ba9c355f3811777060e05d9f86f50
heqinfj <heqinfj@163.com>
2021-08-08 14:51:37 +0800

    运行测试工程

--- src/main/java/com/ecommerce/order/about/AboutController.java
+++ src/main/java/com/ecommerce/order/about/AboutController.java
@@ -1,5 +1,7 @@
 package com.ecommerce.order.about;
 
+import com.ecommerce.order.common.logging.AutoNamingLoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,8 +22,12 @@ public class AboutController {
         this.environment = environment;
     }
 
+    private static final Logger logger = AutoNamingLoggerFactory.getLogger();
+
     @GetMapping
     public AboutRepresentation about() {
+        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+        logger.info("about...");
         String buildNumber = environment.getProperty("buildNumber");
         String buildTime = environment.getProperty("buildTime");
         String gitRevision = environment.getProperty("gitRevision");
